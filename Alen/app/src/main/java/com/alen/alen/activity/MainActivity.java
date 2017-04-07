0531cea39b513e3c8cdc74c16703eb41c7050373
Alen <baqrmvj@qq.com>
2017-04-07 15:09:53 +0800

    点赞效果

--- Alen/app/src/main/java/com/alen/alen/activity/MainActivity.java
+++ Alen/app/src/main/java/com/alen/alen/activity/MainActivity.java
@@ -3,29 +3,61 @@ package com.alen.alen.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.alen.alen.R;
 import com.alibaba.view.BubblingView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private View mVLike;
+    private Button mVLike;
     private BubblingView mBubblingView;
 
+    private int[] mResIds = {R.drawable.flower,
+            R.drawable.shape_circle_blue,
+            R.drawable.shape_circle_green,
+            R.drawable.shape_circle_red,
+            R.drawable.shape_square_black,
+            R.drawable.shape_square_blue,
+            R.drawable.shape_square_orange};
+
+    private int mIndex = 0;
+
+    private final Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            showFlower();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mVLike = findViewById(R.id.btn_like);
+        mVLike = (Button) findViewById(R.id.btn_like);
         mBubblingView = (BubblingView) findViewById(R.id.bubbling_view);
 
         mVLike.setOnClickListener(this);
     }
 
+    private void showFlower() {
+        mVLike.setText(mIndex + "赞");
+        Random random = new Random();
+        mBubblingView.addBubblingItem(mResIds[random.nextInt(mResIds.length - 1)]);
+        mVLike.postDelayed(mRunnable, 500);
+    }
+
     @Override
     public void onClick(View view) {
-        mBubblingView.addBubblingItem(R.drawable.shape_circle);
+        if (mIndex == 0) {
+            mIndex = 1;
+            mVLike.postDelayed(mRunnable, 100);
+        } else {
+            mIndex = 0;
+            mVLike.removeCallbacks(mRunnable);
+        }
     }
 }
