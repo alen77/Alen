a4a0d862a570286148306cd1127736bec57cf42e
Alen <baqrmvj@qq.com>
2017-04-07 13:59:35 +0800

    新增一个点赞动画控件

--- Alen/app/src/main/java/com/alen/alen/activity/MainActivity.java
+++ Alen/app/src/main/java/com/alen/alen/activity/MainActivity.java
@@ -1,15 +1,31 @@
 package com.alen.alen.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.alen.alen.R;
+import com.alibaba.view.BubblingView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View mVLike;
+    private BubblingView mBubblingView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVLike = findViewById(R.id.btn_like);
+        mBubblingView = (BubblingView) findViewById(R.id.bubbling_view);
+
+        mVLike.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        mBubblingView.addBubblingItem(R.drawable.shape_circle);
     }
 }
