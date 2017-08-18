ba5c36e8cf1ac6853f16719183d583daa80b4cf2
Alen <baqrmvj@qq.com>
2017-08-18 14:33:18 +0800

    更多kotlin尝试与时间选择器

--- Alen/app/src/main/java/com/alen/alen/kotlin/KotlinTestActivity.kt
+++ Alen/app/src/main/java/com/alen/alen/kotlin/KotlinTestActivity.kt
@@ -1,8 +1,10 @@
 package com.alen.alen.kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import com.alen.alen.R
 import com.alen.alen.activity.BaseActivity
+import com.alen.alen.activity.TimePickerActivity
 import kotlinx.android.synthetic.main.activity_kotlin_test.*
 
 class KotlinTestActivity : BaseActivity() {
@@ -15,7 +17,14 @@ class KotlinTestActivity : BaseActivity() {
     }
 
     fun initView() {
-        val test : String = "测试Kotlin"
+        val test: String = "测试Kotlin"
         tv_title.text = test
+        tv_title.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    fun showDialog() {
+        startActivity(Intent(this, TimePickerActivity::class.java))
     }
 }
